d42b6bc4a4f122f3db4cda2467e9ba2e3896add1
visakh075 <visakh075@gmail.com>
2022-02-10 12:05:19 +0530

    brow.js and tune function for pack

--- scripts/flock.js
+++ scripts/flock.js
@@ -14,6 +14,11 @@ class bird
       // this.color=Math.random(colors);
       this.max_vel=4;
       this.max_acc=.2;
+      
+      this.cW=1;
+      this.aW=1.5;
+      this.sW=1.5;
+
       this.pos=new p5.Vector(Math.random(),Math.random(0));
       this.pos.mult(500);
       this.vel=p5.Vector.random2D().mult(5);
@@ -21,9 +26,9 @@ class bird
     }
     update()
     {
-        var sep=this.separation().mult(1.5);
-        var ali=this.align().mult(1.5);
-        var coh=this.cohision().mult(1);
+        var sep=this.separation().mult(this.sW);
+        var ali=this.align().mult(this.aW);
+        var coh=this.cohision().mult(this.cW);
         
         var total=p5.Vector.add(coh,ali);
         total.add(sep);
@@ -69,6 +74,7 @@ class bird
         var st=this.scale*Math.sin(theta)/3;
         var ct=this.scale*Math.cos(theta)/3;
         
+        this.p5.fill("#5294e2")
         this.p5.stroke("#5294e2");
         //this.p5.stroke(this.color);
         this.p5.beginShape();
@@ -76,6 +82,7 @@ class bird
         this.p5.vertex(x+st,y-ct);
         this.p5.vertex(x+6*ct,y+6*st);
         this.p5.endShape(this.p5.CLOSE);
+        this.p5.noFill();
 
 
         //circle(this.pos.x,this.pos.y,this.perc_rad);
@@ -88,7 +95,7 @@ class bird
     {
       var d=this.p5.dist(this.pos.x,this.pos.y,birds[i].pos.x,birds[i].pos.y);
       //var d=400;
-      if(d<this.perc_rad && this!=birds[i])
+      if(d<this.perc_rad && this!=birds[i] && d!=0)
       {
         this.friends.push([birds[i],d]);
       }
@@ -130,7 +137,6 @@ class bird
   }
   return(desired);
   }
-
   cohision()
   {
     var desired=new p5.Vector(0,0);
@@ -177,10 +183,32 @@ class world
             this.birds[i].update();
         }
     }
+    tune(_aW,_sW,_cW,m_acc,m_vel)
+    {
+      for(var i=0;i<this.num;i++)
+      {
+        this.birds[i].aW=_aW;
+        this.birds[i].sW=_sW;
+        this.birds[i].cW=_cW;
+        this.birds[i].max_acc=m_acc;
+        this.birds[i].max_vel=m_vel;
+      }
+    }
+    tune_normal()
+    {
+      for(var i=0;i<this.num;i++)
+      {
+        this.birds[i].aW=1.5;
+        this.birds[i].sW=1.5;
+        this.birds[i].cW=1;
+        this.birds[i].max_acc=.2;
+        this.birds[i].max_vel=4;
+      }
+    }
 
 }
-var dW=document.getElementById("gol").offsetWidth;
-var dH=document.getElementById("gol").offsetHeight;
+var dW=document.getElementById("flock_sim").offsetWidth;
+var dH=document.getElementById("flock_sim").offsetHeight;
 dens=100;
 var dens=Math.min(Math.floor(dH*dW/2000),200);
 const s = (sketch) => {
@@ -191,7 +219,7 @@ const s = (sketch) => {
     sketch.setup = () => {
         sketch.p=sketch.createCanvas(dW, dH);
         sketch.run=true;
-        sketch.p.parent("gol");
+        sketch.p.parent("flock_sim");
         sketch.frameRate(30);
         sketch.pack=new world(sketch.width,sketch.height,dens,10,sketch);
     };
@@ -207,8 +235,8 @@ const s = (sketch) => {
      };
      sketch.windowResized = () =>
      {
-        var dW=document.getElementById("gol").offsetWidth;
-        var dH=document.getElementById("gol").offsetHeight;
+        var dW=document.getElementById("flock_sim").offsetWidth;
+        var dH=document.getElementById("flock_sim").offsetHeight;
         sketch.resizeCanvas(dW, dH);
      };
      sketch.keyPressed = () =>
@@ -221,6 +249,4 @@ const s = (sketch) => {
         }
      };
   };
-  
-
-  let myp5 = new p5(s);
+let flock_world_sim = new p5(s);
