cca8766727a83f2a627ff70c7968fd68ddf603c9
visakh075 <visakh075@gmail.com>
2022-02-15 15:08:01 +0530

    progress bar working

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,6 +1,25 @@
-$("#tune_bar").click(
-    function()
-    {
-        console.log("clicke");
-    }
-);
+console.log("hai");
+$(document).ready(main);
+
+function main()
+{
+    
+    $(".tune_bar").mousemove(
+        function(event)
+        {
+            var offset = $(this).offset();
+            var top = offset.top;
+            var left = offset.left;
+            
+            var t_w=$(this).width();
+            var t_h=$(this).height();
+
+            if(event.buttons==1)
+            {
+                $(this).find(".progress").css("width",(event.pageX-left));
+                console.log($(this).attr('id'));
+            }
+        }
+    );
+
+}
